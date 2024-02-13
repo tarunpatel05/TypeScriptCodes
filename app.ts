c3e88fa8777450e404f974dbcd3ce0ad34a144cf
tarun_patel <tarun_patel05@yahoo.co.in>
2024-02-13 12:07:03 +0530

    adding more changes

--- app.ts
+++ app.ts
@@ -18,7 +18,35 @@
 // +++++++++++++++++++++++++++++ literal types ++++++
 
 // console.log('Time to get started');
-function combine(input1: number | string, input2: number | string, resultConversion:'as-number'|'as-string') {
+// function combine(input1: number | string, input2: number | string, resultConversion:'as-number'|'as-string') {
+//     let result;
+//     if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+//       result = +input1 + +input2;
+//     } else {
+//       result = input1.toString() + input2.toString();
+//     }
+//     return result
+//     // if(resultConversion==='as-number'){
+//     //     return +result;
+//     //     // return parseFloat(result);
+//     // }else{
+//     //     return result.toString();
+//     // }
+  
+//   }
+//   const combinedAges = combine(30, 26, 'as-number');
+//   console.log(combinedAges);
+
+//   const combinedStringAges = combine('30', '26', 'as-number');
+//   console.log(combinedStringAges);
+  
+//   const combinedNames = combine("Tarun", "Patel", 'as-string');
+//   console.log(combinedNames);
+
+// ========================== Type Aliases/Custom Types
+type Combinable = number|string;
+
+function combine(input1: Combinable, input2: Combinable, resultConversion:'as-number'|'as-string') {
     let result;
     if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
       result = +input1 + +input2;
@@ -26,12 +54,6 @@ function combine(input1: number | string, input2: number | string, resultConvers
       result = input1.toString() + input2.toString();
     }
     return result
-    // if(resultConversion==='as-number'){
-    //     return +result;
-    //     // return parseFloat(result);
-    // }else{
-    //     return result.toString();
-    // }
   
   }
   const combinedAges = combine(30, 26, 'as-number');
