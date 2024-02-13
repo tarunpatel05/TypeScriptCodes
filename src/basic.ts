bd01cba3fc17ffbbc6e186c043642bcc82b039c9
tarun_patel <tarun_patel05@yahoo.co.in>
2024-02-14 00:51:01 +0530

    changed the folder structure

--- src/basic.ts
+++ src/basic.ts
@@ -9,6 +9,7 @@ function add(n1:number, n2:number, showResult:boolean, phrase: string){
     } else{
         return result;
     }
+    return;
 }
 
 let numberX: number
--- src/functions.ts
+++ src/functions.ts
@@ -37,28 +37,28 @@
 
 /// ++++++++++++++ Function Type and callback
 
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+function add1(n1: number, n2: number): number {
+return n1 + n2;
 }
 
-function printResult(num: number): void {
-  console.log("Result: " + num);
-  return;
-}
+// function printResult(num: number): void {
+// console.log("Result: " + num);
+// return;
+// }
 
 function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
+const result = n1 + n2;
+cb(result);
 }
 
-console.log(printResult(add(5, 12))); //retrun undefined
+// console.log(printResult(add(5, 12))); //retrun undefined
 
 let combineValues: (a: number, b: number) => number; //function type, specific function
 
-combineValues = add; // add is function
+combineValues = add1; // add is function
 console.log(combineValues(8, 8));
 
 // addAndHandle(10,20,()=> {})  // ()=> {} is anonymous function
 addAndHandle(10,20,(result)=> {
-    console.log(result)
+console.log(result)
 })  // ()=> {} is anonymous function
